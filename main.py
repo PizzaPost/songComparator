abba50c27591e367d42d5dd00ca005baf388cf7f
Phil <pizzapost.mail@gmail.com>
2025-10-03 18:37:54 +0200

    second prototype of video playing

--- main.py
+++ main.py
@@ -9,29 +9,35 @@ def run():
     pygame.display.set_icon(pygame.image.load("resources/assets/icon.png"))
     clock=pygame.time.Clock()
     video=None
-    def play_track(url=None, filename=None):
-        global video
-        if url:
-            video=pyvidplayer2.Video(url, youtube=True)
-            video.preview()
-        elif filename:
-            video=pyvidplayer2.Video(filename)
-            video.preview()
     while running:
         pg.fill((0,0,0))
+        keys=pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 running=False
-        keys=pygame.key.get_pressed()
+            elif event.type==pygame.KEYUP:
+                if event.key==pygame.K_k:
+                    if video:
+                        video.toggle_pause()
         if keys[pygame.K_SPACE]:
-            play_track(url="https://youtu.be/Qt5wB7KXSaM?si=Yq5hiKMj26jyFSe4")
+            url="https://youtu.be/Qt5wB7KXSaM"
+            video = pyvidplayer2.Video(url, youtube=True)
+            video.resize((width, height))
+        elif keys[pygame.K_l]:
+            if video:
+                video.seek(5)
+        elif keys[pygame.K_j]:
+            if video:
+                video.seek(-5)
+        if esc==215:
+            running=False
+        if video:
+            video.draw(pg, (0,0))
         if keys[pygame.K_ESCAPE]:
             esc+=1
             pg.fill((255,0,0), (0, height-50, width//200*esc, 50))
         else:
             esc=0
-        if esc==215:
-            running=False
         pygame.display.update()
         clock.tick(120)
     pygame.quit()
